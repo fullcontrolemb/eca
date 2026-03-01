be3fde7b5a63e1e14cb0facc3a22069d54d38662
evaldobarros@gmail.com <evaldobarros@gmail.com>
2026-03-01 14:31:18 -0300

    atualização

--- invoice-web-app/database.py
+++ invoice-web-app/database.py
@@ -1,5 +1,6 @@
 from supabase import create_client
 import streamlit as st
+import json
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
@@ -8,11 +9,15 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 def save_token(email, token_json):
-    supabase.table("users_tokens").upsert({
-        "email": email,
-        "token": token_json
-    }).execute()
+    response = supabase.table("users_tokens").upsert(
+        {
+            "email": email,
+            "token": json.dumps(token_json)
+        },
+        on_conflict="email"
+    ).execute()
 
+    return response
 
 def get_token(email):
     response = supabase.table("users_tokens") \
@@ -21,6 +26,6 @@ def get_token(email):
         .execute()
 
     if response.data:
-        return response.data[0]["token"]
+        return json.loads(response.data[0]["token"])
 
     return None
